454b42595d5ae3f842f51b2200e81174e1ef34f7
jeonjiwon <wldjs08@gmail.com>
2020-05-20 22:46:10 +0900

    class page update

--- web_vue/src/vueClass/machine.vue
+++ web_vue/src/vueClass/machine.vue
@@ -4,8 +4,31 @@
         
         <div class=recordList>
             <h2>머신러닝 실무</h2>
-            
         </div>
+
+        <section class="panel">
+            <header class="panel-heading">
+                <div class="panel-actions">
+                    <a href="#" class="fa fa-caret-down"></a>
+                    <a href="#" class="fa fa-times"></a>
+                </div>
+
+                <h3 class="panel-title">강의 소개</h3>
+            </header>
+            <p class="panel-subtitle" style="font-weight: bold;">교과목 개요</p>
+            <div class="panel-body">
+                <p class="text-justify">인공지능(AI) 머신러닝 분야에서 대표적인 알고리즘에 대한 이해와 학습을 하며, 파이썬과
+                    데이터 관련 라이브러리, 크롤링, 머신러닝 라이브러리 등을 통해 AI서비스를 학습한다.
+                </p>
+            </div>
+            <hr width =100% color="blue" align="left" size=30/>
+            <p class="panel-subtitle" style="font-weight: bold;">교육 목표</p>
+            <div class="panel-body">
+                <p class="text-justify">머신러닝 분야에서 대표적인 알고리즘에 대한 이해와 학습을 하며, 파이썬과
+                    데이터 관련 라이브러리, 크롤링, 머신러닝 라이브러리 등을 통해 AI서비스를 학습한다.
+                </p>
+            </div>
+        </section>
     </div>
 </template>
 
@@ -36,5 +59,14 @@ export default {
 h2 {
     width: 400px;
     margin: 0 0 2% 0;
+}
+
+.panel {
+    margin-left: 7%;
+    width: 80%;
+}
+
+p {
+    margin-top: 15px;
 }
 </style>
--- web_vue/src/vueClass/programing.vue
+++ web_vue/src/vueClass/programing.vue
@@ -4,8 +4,30 @@
         
         <div class=recordList>
             <h2>지능형 프로그래밍</h2>
-            
         </div>
+
+        <section class="panel">
+            <header class="panel-heading">
+                <div class="panel-actions">
+                    <a href="#" class="fa fa-caret-down"></a>
+                    <a href="#" class="fa fa-times"></a>
+                </div>
+
+                <h3 class="panel-title">강의 소개</h3>
+            </header>
+            <p class="panel-subtitle" style="font-weight: bold;">교과목 개요</p>
+            <div class="panel-body">
+                <p class="text-justify">인공지능 기초지식과 응용지식을 강의하고 AI 기술을 이용한 관련 프로그램을 분석 및 구현해 본다.
+                </p>
+            </div>
+            <hr width =100% color="blue" align="left" size=30/>
+            <p class="panel-subtitle" style="font-weight: bold;">교육 목표</p>
+            <div class="panel-body">
+                <p class="text-justify">1. 인공지능 시스템 구현 기술들을 알 수 있다.<br>2. 지능형 시스템 구현 원리를 알 수 있다.
+                    <br>3. 간단한 지능형 프로그램을 구현 할 수 있다.
+                </p>
+            </div>
+        </section>
     </div>
 </template>
 
@@ -36,5 +58,14 @@ export default {
 h2 {
     width: 400px;
     margin: 0 0 2% 0;
+}
+
+.panel {
+    margin-left: 7%;
+    width: 80%;
+}
+
+p {
+    margin-top: 15px;
 }
 </style>
