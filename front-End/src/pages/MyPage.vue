acda5dee67a5278189ac55968fb58fe744366e5c
jeonjiwon <wldjs08@gmail.com>
2020-06-24 12:38:21 +0900

    my page modify -ing

--- front-End/src/pages/MyPage.vue
+++ front-End/src/pages/MyPage.vue
@@ -2,20 +2,20 @@
   <div>
     <div class="page-header clear-filter" filter-color="orange">
       <div class="container">
-        <h3 class="title">Ryan Scheinder</h3>
-        <p class="category">Photographer</p>
+        <h3 class="title">나의 정보</h3>
+        <!-- <p class="category">Photographer</p> -->
         <div class="content">
           <div class="social-description">
-            <h2>26</h2>
-            <p>Comments</p>
+            <h2 style="margin-bottom: 5px;">학번</h2>
+            <h4 style="margin-bottom: 30px; margin-top: 0px;">   202046010</h4>
           </div>
           <div class="social-description">
-            <h2>26</h2>
-            <p>Comments</p>
+            <h2 style="margin-bottom: 5px;">이름</h2>
+            <h4 style="margin-bottom: 30px; margin-top: 0px;">   전지원</h4>
           </div>
           <div class="social-description">
-            <h2>48</h2>
-            <p>Bookmarks</p>
+            <h2 style="margin-bottom: 5px;">학과</h2>
+            <h4 style="margin-bottom: 30px; margin-top: 0px;">   컴퓨터시스템공학과</h4>
           </div>
         </div>
       </div>
